9fd4df4211cde2a9cfa8da925c51679d0192532e
Ali Masarwe <33619630+AliMasarweh@users.noreply.github.com>
2019-11-26 19:02:21 +0200

    Documentation

--- OOP_Assignments/src/assignment3/Ex3A.java
+++ OOP_Assignments/src/assignment3/Ex3A.java
@@ -1,12 +1,12 @@
 package assignment3;
 
 /**
- * Ex3A is a class to cover for Ex3_tester class's faultiness
+ * Ex3A is a class to cover for Ex3_tester class's faulty implementation
  * Utilizing the threads properties of semi-parallel computing
  * to run an internal function while keeping track of it's running 
- * time to determine whether it took an unreasonable without yielding an 
- * answer or it still has more time according the prediction of the 
- * function user to determine if the number is prime.
+ * time to determine whether it took an unreasonable without returning an 
+ * answer or the flag finished has been triggered in due time of the prediction 
+ * by the function isPrime which used to determine if the number is prime.
  * @author Ali Masarweh
  * 
  */
@@ -15,6 +15,16 @@ public class Ex3A implements Runnable{
 	boolean ans = false;
 	boolean finished = false;
 	long num = -1;
+	final static int minimal_time = 5000;
+	int time;
+	
+	public Ex3A(){
+		time = minimal_time;	
+	}
+	
+	public Ex3A(int time){
+		this.time = Math.max(time, minimal_time);	
+	}
 
 	/**
 	 * Wrapper of the function Ex3_tester.isPrimes(long n) to deal with
@@ -22,7 +32,7 @@ public class Ex3A implements Runnable{
 	 * @param n the given number argument to be checked
 	 * @param d the given time in seconds argument to break 
 	 * out of internal function, in this case we multiply this time argument 
-	 * by 5 to account for slower systems
+	 * by time member to account for slower systems
 	 * @return true if the given number is prime, otherwise, false
 	 * @throws RuntimeException in case of internal function failure
 	 */
@@ -33,7 +43,7 @@ public class Ex3A implements Runnable{
 		long startingTime = System.currentTimeMillis();
 		thread.start();
 		while(!finished) {
-			if(System.currentTimeMillis() - d*5000 > startingTime) {
+			if(System.currentTimeMillis() - d*time > startingTime) {
 				thread.stop();
 				throw new RuntimeException("Internal function is stuck!");
 			}
